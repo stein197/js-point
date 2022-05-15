839ab76b9482aeb9c9f7197877d717004ddff1e4
Nail' Gafarov <nil20122013@gmail.com>
2022-05-15 18:27:56 +0500

    Added generic length, getDistance() method

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import {Cloneable, Equalable} from "@stein197/ts-util";
 
-export default class Point implements Cloneable<Point>, Equalable<Point> {
+export default class Point<N extends number> implements Cloneable<Point<N>>, Equalable<Point<N>> {
 
-	public readonly vector: number[];
+	public readonly vector: ConstArray<number, N>;
 
-	public constructor(...vector: number[]) {
+	public get x(): number {
+		return this.vector[0];
+	}
+
+	public get y(): number {
+		return this.vector[1];
+	}
+
+	public get z(): number {
+		return this.vector[2];
+	}
+
+	public constructor(...vector: ConstArray<number, N>) {
 		this.vector = vector;
 	}
 
@@ -12,11 +24,34 @@ export default class Point implements Cloneable<Point>, Equalable<Point> {
 		return `[${this.vector.join(", ")}]`;
 	}
 
-	public clone(): Point {
-		return new Point(...this.vector.slice());
+	public clone(): Point<N> {
+		// @ts-ignore
+		return new Point(...this.vector);
 	}
 
-	public equals(obj: Point): boolean {
+	public equals(obj: Point<N>): boolean {
 		return this.vector.length === obj.vector.length && this.vector.every((n, i) => obj.vector[i] === n);
 	}
+
+	public getDistance(point: Point<N>): number {
+		return Math.sqrt(this.vector.reduce((prev, v, i) => prev + (v - point.vector[i]) ** 2, 0));
+	}
+
+	public static create<N extends number>(...vector: ConstArray<number, N>): PointExclude<N> {
+		// @ts-ignore
+		return new Point(...vector);
+	}
 }
+
+type ConstArray<T, L extends number> = Array<T> & {
+	0: T,
+	readonly length: L
+}
+
+type PointExclude<N extends number> = Omit<Point<N>, N extends 0 ? (
+	"x" | "y" | "z"
+) : N extends 1 ? (
+	"y" | "z"
+) : N extends 2 ? (
+	"z"
+) : never>;
