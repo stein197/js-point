9c3b671696f0ca2c03480dca52c08c07418c8821
Nail' Gafarov <nil20122013@gmail.com>
2022-05-15 16:23:44 +0500

    Implemented interfaces

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 import {Cloneable, Equalable} from "@stein197/ts-util";
 
-export default class Point implements Cloneable<Point>, Equalable<Point> {}
+export default class Point implements Cloneable<Point>, Equalable<Point> {
+
+	public constructor(public readonly vector: number[]) {}
+
+	public clone(): Point {
+		return new Point(this.vector.slice());
+	}
+
+	public equals(obj: Point): boolean {
+		if (this.vector.length !== obj.vector.length)
+			return false;
+		return this.vector.every((n, i) => obj.vector[i] === n);
+	}
+}
