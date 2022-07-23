c0b1ea9bb0ff42afb66c26af04718b93bf9f9dec
Nail' Gafarov <nil20122013@gmail.com>
2022-07-23 14:17:52 +0500

    Reorganized tests

--- test.ts
+++ test.ts
@@ -30,20 +30,26 @@ mocha.describe("Point", () => {
 		new Point<1>(1).toString().should.be.equal("[1]");
 		new Point<2>(1, 1.5).toString().should.be.equal("[1, 1.5]");
 	});
-	mocha.it("clone() should return an object with the same data", () => new Point<2>(1, 2).clone().vector.should.be.eql([1, 2]));
-	mocha.it("clone() should return a different reference", () => {
-		let p1 = new Point(1);
-		let p2 = p1.clone();
-		(p1 === p2).should.be.false();
+	mocha.describe("clone()", () => {
+		mocha.it("clone() should return an object with the same data", () => new Point<2>(1, 2).clone().vector.should.be.eql([1, 2]));
+		mocha.it("clone() should return a different reference", () => {
+			let p1 = new Point(1);
+			let p2 = p1.clone();
+			(p1 === p2).should.be.false();
+		});
 	});
-	mocha.it("equals() should return true for equal points", () => new Point<2>(1, 2).equals(new Point(1, 2)).should.be.true());
-	mocha.it("equals() should return false for unequal points", () => new Point<2>(1, 2).equals(new Point(2, 3)).should.be.false());
+	mocha.describe("equals()", () => {
+		mocha.it("equals() should return true for equal points", () => new Point<2>(1, 2).equals(new Point(1, 2)).should.be.true());
+		mocha.it("equals() should return false for unequal points", () => new Point<2>(1, 2).equals(new Point(2, 3)).should.be.false());
+	});
 	mocha.it("getDistance() should return correct result", () => {
 		new Point<2>(1, 1).getDistance(new Point(2, 2)).should.be.approximately(1.41, 0.01);
 		new Point<3>(1, 2, 3).getDistance(new Point(4, 5, 6)).should.be.approximately(5.19, 0.01);
 	});
 	mocha.it("create() works the same as the constructor", () => Point.create<2>(2, 3).vector.should.be.eql([2, 3]));
-	mocha.it("interpolate() should return the first point when t is 0", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), 0).vector.should.be.eql([0, 0, 0]));
-	mocha.it("interpolate() should return the second point when t is 1", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), 1).vector.should.be.eql([5, 5, 5]));
-	mocha.it("interpolate() should return correct value", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), .2).vector.should.be.eql([1, 1, 1]));
+	mocha.describe("interpolate()", () => {
+		mocha.it("interpolate() should return the first point when t is 0", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), 0).vector.should.be.eql([0, 0, 0]));
+		mocha.it("interpolate() should return the second point when t is 1", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), 1).vector.should.be.eql([5, 5, 5]));
+		mocha.it("interpolate() should return correct value", () => Point.interpolate(new Point<3>(0, 0, 0), new Point<3>(5, 5, 5), .2).vector.should.be.eql([1, 1, 1]));
+	});
 });
