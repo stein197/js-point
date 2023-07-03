9ba11ac7de5f90c5d8f824e44385c1b1a64518e4
Nail Gafarov <nil20122013@gmail.com>
2023-07-03 14:42:27 +0500

    Added Point2D, Point3D

---
+++ src/Point2D.ts
@@ -0,0 +1,29 @@
+import Point from "./Point";
+
+// TODO: Tests, docs
+export = class Point2D extends Point {
+
+	public get x(): number {
+		return this.vector[0];
+	}
+
+	public set x(value: number) {
+		this.vector[0] = value;
+	}
+
+	public get y(): number {
+		return this.vector[1];
+	}
+
+	public set y(value: number) {
+		this.vector[1] = value;
+	}
+
+	public constructor(x: number, y: number) {
+		super(x, y);
+	}
+
+	public toPlain(): {x: number; y: number;} {
+		return {x: this.x, y: this.y};
+	}
+}
---
+++ src/Point3D.ts
@@ -0,0 +1,22 @@
+import Point2D from "./Point2D";
+
+// TODO: Tests, docs
+export = class Point3D extends Point2D {
+
+	public get z(): number {
+		return this.vector[2];
+	}
+
+	public set z(value: number) {
+		this.vector[2] = value;
+	}
+
+	public constructor(x: number, y: number, z: number) {
+		super(x, y);
+		this.vector.push(z);
+	}
+
+	public toPlain(): { x: number; y: number; z: number; } {
+		return {x: this.x, y: this.y, z: this.z};
+	}
+}
